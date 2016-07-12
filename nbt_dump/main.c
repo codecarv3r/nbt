81f067c2107f7de54d7ea4a6ad73f0d3839d8187
Silas Schwarz <silas.zeta@gmail.com>
2016-07-12 13:33:47 -0600

    oops

--- nbt_dump/main.c
+++ nbt_dump/main.c
@@ -1,10 +1,31 @@
-//
-//  main.c
-//  nbt_dump
-//
-//  Created by Silas Schwarz on 7/11/16.
-//  Copyright © 2016 ZCodeMT. All rights reserved.
-//
+/*
+ *   ___    __ __  ____ ________
+ *  |   \  |  |  |/ _  \        |
+ *  |    \ |  |    (_) /__    __|
+ *  |  |\ \|  |     _  \  |  |
+ *  |  | \    |    (_) |  |  |
+ *  |__|  \___|__|\____/  |__|
+ *
+ *  main.c
+ *  This file is part of nbt.
+ *
+ *  Created by Silas Schwarz on 7/11/16.
+ *  Copyright (c) 2016 ZCodeMT LLC.
+ *
+ *  nbt is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  nbt is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with nbt.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 #include <stdlib.h>
 #include <stdio.h>
